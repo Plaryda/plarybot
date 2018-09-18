0ee6f7b3546ac4a5bb207822913b60aede83434b
Plaryda <42467740+Plaryda@users.noreply.github.com>
2018-09-18 18:19:08 +0800

    Add files via upload

---
+++ Music.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+# """music commands for Plaryboat
+#   credits to Lucas Kumara for all heads up"""
+
+class Music:
+    def __init__(self,bot): #self and the bot variable
+        self.bot = bot
+
+    async def on_message_delete(self,message):
+        await self.bot.send_message(message.channel,'message deleted.')
+
+
+    @commands.command()
+    async def oof(self):
+        await self.bot.say('pong')
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Music(Bot))
